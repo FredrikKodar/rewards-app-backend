213e1f2656894e691d6f1bd356f4e0044d424df3
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2026-01-07 10:35:04 +0100

    Add mapping to register child user

--- src/main/java/com/fredande/rewardsappbackend/controller/AuthenticationController.java
+++ src/main/java/com/fredande/rewardsappbackend/controller/AuthenticationController.java
@@ -1,12 +1,15 @@
 package com.fredande.rewardsappbackend.controller;
 
+import com.fredande.rewardsappbackend.CustomUserDetails;
 import com.fredande.rewardsappbackend.dto.AuthResponse;
+import com.fredande.rewardsappbackend.dto.ChildRegistrationRequest;
 import com.fredande.rewardsappbackend.dto.LoginRequest;
-import com.fredande.rewardsappbackend.dto.RegistrationRequest;
+import com.fredande.rewardsappbackend.dto.ParentRegistrationRequest;
 import com.fredande.rewardsappbackend.service.AuthenticationServiceDef;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,8 +31,8 @@ public class AuthenticationController {
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest loginRequest) {
         UserDetails userDetails = authService.authenticate(
-                loginRequest.getEmail(),
-                loginRequest.getPassword());
+                loginRequest.email(),
+                loginRequest.password());
         String token = authService.generateToken(userDetails);
         String roles = userDetails.getAuthorities().toString();
         AuthResponse authResponse = new AuthResponse(token, EXPIRES_IN, roles);
@@ -37,8 +40,15 @@ public class AuthenticationController {
     }
 
     @PostMapping("/register")
-    public ResponseEntity<String> register(@RequestBody @Valid RegistrationRequest registrationRequest) {
-        authService.register(registrationRequest);
+    public ResponseEntity<String> registerParent(@RequestBody @Valid ParentRegistrationRequest parentRegistrationRequest) {
+        authService.registerParent(parentRegistrationRequest);
+        return ResponseEntity.status(HttpStatusCode.valueOf(201)).body("User registered successfully");
+    }
+
+    @PostMapping("/register-child")
+    public ResponseEntity<String> registerChild(@RequestBody @Valid ChildRegistrationRequest childRegistrationRequest,
+                                                @AuthenticationPrincipal CustomUserDetails customUserDetails) {
+        authService.registerChild(childRegistrationRequest, customUserDetails);
         return ResponseEntity.status(HttpStatusCode.valueOf(201)).body("User registered successfully");
     }
 
