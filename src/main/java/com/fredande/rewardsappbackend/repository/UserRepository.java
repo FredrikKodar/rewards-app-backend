a22b2aa379e991362b453c01ba6f7cee00c9c371
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2026-01-09 08:27:55 +0100

    Add method to get a users associated Child users

--- src/main/java/com/fredande/rewardsappbackend/repository/UserRepository.java
+++ src/main/java/com/fredande/rewardsappbackend/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.fredande.rewardsappbackend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -12,5 +13,7 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 
     Optional<User> findByEmailOrUsername(String email, String username);
+
+    List<User> findAllByParent(User user);
 
 }
