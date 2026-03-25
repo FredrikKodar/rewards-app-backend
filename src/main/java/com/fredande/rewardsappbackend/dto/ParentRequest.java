5ec681be2de7229ca663fba474a6ecb4f891bdb6
Fredrik <3tomu24.andefr@folkuniversitetet.nu>
2026-03-25 08:44:43 +0100

    chore: make record

--- src/main/java/com/fredande/rewardsappbackend/dto/ParentRequest.java
+++ src/main/java/com/fredande/rewardsappbackend/dto/ParentRequest.java
@@ -3,25 +3,20 @@ package com.fredande.rewardsappbackend.dto;
 import com.fredande.rewardsappbackend.constants.ValidationConstants;
 import jakarta.validation.constraints.*;
 
-public class ParentRequest {
-
-    private final int minPasswordLength = 8;
-    private final int maxPasswordLength = 40;
-    private final String emailRegEx = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
-
-
-    @Pattern(regexp = emailRegEx,
-            message = "Malformed email address")
-    private String email;
-    @Size(min = minPasswordLength,
-            max = maxPasswordLength,
-            message = "Password must be " + minPasswordLength + " to " + maxPasswordLength + " characters")
-    private String password;
-    @NotBlank(message = "First name is required")
-    private String firstName;
+public record ParentRequest(
+        @NotNull
         @Pattern(regexp = ValidationConstants.EMAIL_REGEX,
+                message = "Malformed email address")
+        String email,
+
+        @NotNull
         @Size(min = ValidationConstants.MIN_PASSWORD_LENGTH,
                 max = ValidationConstants.MAX_PASSWORD_LENGTH,
+                message = "Password must be " + ValidationConstants.MIN_PASSWORD_LENGTH + " to " + ValidationConstants.MAX_PASSWORD_LENGTH + " characters")
+        String password,
+
+        @NotBlank(message = "First name is required")
+        String firstName,
 
         @NotBlank(message = "Last name is required")
         String lastName
